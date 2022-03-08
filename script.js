51c02c94cfabd59e4bee0762df98caf52d5096e3
AndrisJa <100909331+AndrisJa@users.noreply.github.com>
2022-03-08 13:15:36 +0200

    wtf

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const vardi=['Jana Krūms', 'Alfrēds Bērziņš', 'Renāte Kasparoviča', 'Laura Ose', 'Eduards Priede'];
 const balvas=['Dators', 'Telefons', 'Mašīna', 'Lāpsta', 'Grābeklis'];
 const naudaKopa=1000000;
+
 let uzvaretajuSkaits=5;
 let rindas=document.querySelector('.rindas'); //pievieno mainīgo
-
+rindas.innerHTML ='';
 
 for(let i=0;i<uzvaretajuSkaits;i++){
-    
 let rand=Math.random()*vardi.length;  //vārdu skaits mainīgs
 rand=Math.floor(rand); //noapaļo uz leju
-let uzvaretajs=vardi[rand]; //console.log vietā
 
 
+let uzvaretaju = vardi[rand]; //console.log vietā
+let laimesti = balvas[rand];
 
-rindas.innerHTML+=` //pievieno +, lai pieskaitītu nākamo
+
+         rindas.innerHTML +=` 
 <tr>
-<td>${i+1}</td>
-<td>${uzvaretajs}</td>
-</tr>` //izvade vairākās rindās
+    <td>${i + 1}</td>
+    <td>${uzvaretaju}</td>
+    <td>${laimesti}</td>
+</tr>`
 
 }
