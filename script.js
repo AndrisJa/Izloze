28e4119125d748ddd08cf95448361460e2a0da70
AndrisJa <100909331+AndrisJa@users.noreply.github.com>
2022-03-08 10:18:19 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,5 @@
+const vardi=['Jana Krūms', 'Alfrēds Bērziņš', 'Renāte Kasparoviča', 'Laura Ose', 'Eduards Priede'];
+const balvas=['Dators', 'Telefons', 'Mašīna', 'Lāpsta', 'Grābeklis'];
+const naudaKopa=1000000;
+let uzvaretajuSkaits=5;
+console.log(vardi[rand]);
