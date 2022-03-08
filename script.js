e637c3c6157b8bfc32038c56ed6ff3236b14a301
AndrisJa <100909331+AndrisJa@users.noreply.github.com>
2022-03-08 10:40:50 +0200

    pirmais

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const vardi=['Jana Krūms', 'Alfrēds Bērziņš', 'Renāte Kasparoviča', 'Laur
 const balvas=['Dators', 'Telefons', 'Mašīna', 'Lāpsta', 'Grābeklis'];
 const naudaKopa=1000000;
 let uzvaretajuSkaits=5;
+Math.random=Math.random()*5;
+rand=Math.random()*vardi.length;  //vārdu skaits mainīgs
 console.log(vardi[rand]);
